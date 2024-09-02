94b32663c83582da653fd9b42dcf4510cc911f40
Alex CHAMOUX <alex.chamoux@Digital-Grenoble.local>
2024-09-02 12:06:06 +0200

    fin de l'exercice 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,33 @@
 // you can write js here
 console.log('exo-2');
+
+// const myDate = new Date();
+
+let isTesting = true;
+let currentDay;
+let currentHours;
+
+if (isTesting) {
+    currentDay = prompt("Quelle jour sommes nous ? (Lundi => 1, Mardi => 2 etc.. Dimanche => 0");
+    currentHours = prompt("Quelle heure est-il ? (juste le nombre des heures)");
+}else {
+    const mydate = new Date();
+    currentDay = mydate.getDay();
+    currentHours = mydate.getHours();
+    // console.log("Heure actuel :", currentHours);
+}
+
+let weekDay = "Nous sommes en semaine";
+let weekEndDay = "Nous sommes en week-end";
+
+// let currentDay = myDate.getDay();
+// let currentHours = myDate.getHours();
+// console.log("Heure :", currentHours);
+
+if ((currentDay >= 1 && currentHours >= 9) && (currentDay <= 5 && currentHours <= 17)) {
+    console.log(weekDay);
+} else {
+    console.log(weekEndDay);
+}
+
+
