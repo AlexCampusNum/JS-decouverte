3d763ba97be71c65ee3995535c96229857adf1c8
AlexCampusNum <alex.chamoux@le-campus-numerique.fr>
2024-09-02 14:43:59 +0200

    fin de l'exercice 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,23 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+console.log(secretMessage.pop());
+// console.log(secretMessage);
+
+secretMessage.push("to", "program");
+// console.log(secretMessage);
+
+secretMessage.splice(6, 1, 'right');
+// console.log(secretMessage);
+
+secretMessage.shift();
+// console.log(secretMessage);
+
+secretMessage.unshift("Programming");
+// console.log(secretMessage);
+
+secretMessage.splice(5, 4, "know");
+// console.log(secretMessage);
+
+console.log(secretMessage.join(' '));
