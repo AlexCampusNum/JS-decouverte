1eedcac869f20b8042bf0f76e77c6065004cb155
AlexCampusNum <alex.chamoux@le-campus-numerique.fr>
2024-09-04 16:37:36 +0200

    fin du 7.10

---
+++ exo-7/datas2.js
@@ -0,0 +1,126 @@
+let jsonDatas = [
+	{
+		"type": "car",
+		"items": [
+			{
+				"name": "Fiat Punto",
+				"description": "Je suis une voiture",
+				"price": 10000,
+				"quantity": 2,
+				"contact": {
+					"lastName": "Dubois",
+					"firstName": "Martin",
+					"address": "1 Grande Rue 74000 Annecy"
+				}
+			},
+			{
+				"name": "Porsche 911",
+				"description": "Je suis une belle voiture",
+				"price": 80000,
+				"quantity": 0,
+				"contact": {
+					"lastName": "Desbois",
+					"firstName": "Martine",
+					"address": "1 Grande Rue 6000 Lyon"
+				}
+			},
+			{
+				"name": "Peugeot 205",
+				"description": "Je suis une autre voiture",
+				"price": 2000,
+				"quantity": 2,
+				"contact": {
+					"lastName": "Bois",
+					"firstName": "Jade",
+					"address": "1 Grande Rue 38000 Grenoble"
+				}
+			}
+		]
+	},
+	{
+		"type": "house",
+		"items": [
+			{
+				"name": "Villa sur la plage",
+				"description": "Quelle belle vue",
+				"price": 870000,
+				"quantity": 1,
+				"contact": {
+					"lastName": "Lefevre",
+					"firstName": "Claire",
+					"address": "2 Rue de la Mer 75000 Paris"
+				}
+			},
+			{
+				"name": "Maison à la campagne",
+				"description": "Vive le calme",
+				"price": 170000,
+				"quantity": 3,
+				"contact": {
+					"lastName": "Luis",
+					"firstName": "Claire",
+					"address": "2 Rue de la Mer 83000 Toulon"
+				}
+			}
+		]
+	},
+	{
+		"type": "game",
+		"items": [
+			{
+				"name": "Monopoly",
+				"description": "",
+				"price": 30,
+				"quantity": 300,
+				"contact": {
+					"lastName": "Martin",
+					"firstName": "Jean",
+					"address": "3 Rue du Jeu 69000 Lyon"
+				}
+			}
+		]
+	},
+	{
+		"type": "videoGame",
+		"items": [
+			{
+				"name": "Mario Bros",
+				"description": "",
+				"price": 30,
+				"quantity": 200,
+				"contact": {
+					"lastName": "Martin",
+					"firstName": "Jean",
+					"address": "3 Rue du Jeu 69000 Lyon"
+				}
+			}
+		]
+	},
+	{
+		"type": "show",
+		"items": [
+			{
+				"name": "Place VIP concert Metallica",
+				"description": "",
+				"price": 800,
+				"quantity": 10,
+				"contact": {
+					"lastName": "Bernard",
+					"firstName": "Luc",
+					"address": "4 Rue du Concert 75000 Paris"
+				}
+			},
+			{
+				"name": "Entrée au parc Astérix",
+				"description": "",
+				"price": 30,
+				"quantity": 200,
+				"contact": {
+					"lastName": "Bernard",
+					"firstName": "Luc",
+					"address": "4 Rue du Concert 75000 Paris"
+				}
+			}
+		]
+	}
+];
--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -202,7 +202,193 @@ console.log(jsonDatas);
 
 
 //Exercice 8
+// let form = document.getElementById("itemForm");
+//
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//
+//     let name = document.getElementById('name').value;
+//     let type = document.getElementById('type').value;
+//     let description = document.getElementById('description').value;
+//     let price = document.getElementById('price').value;
+//     let quantity = document.getElementById('quantity').value;
+//
+//     let newItem = {
+//         "name": name,
+//         "type": type,
+//         "description": description,
+//         "price": parseFloat(price),
+//         "quantity": parseInt(quantity),
+//     }
+//
+//     jsonDatas.push(newItem);
+//
+//     console.log("Nouvel item ajouté : ", newItem);
+//     console.log("Liste mise à jour : ", jsonDatas);
+//
+//     document.getElementById('itemForm').reset();
+// });
+
+//Exercice 9
+//Unifier tout mon code
+// Gestion de l'affichage des articles selon les filtres et les tris
+let articlesList = document.getElementById("articles-list");
+let button = document.getElementById("btn");
+let check = document.getElementById("stock");
+let nameItem = document.getElementById("nameItem");
+let price = document.getElementById("prix");
+
+function displayArticles(data) {
+    articlesList.innerHTML = ''; // On vide la liste des articles
+
+    let foundArticles = false;
+
+    data.forEach(category => {
+        let categoryType = category.typeTraduit;
+        let categoryItems = category.items
+
+        categoryItems.forEach(article => {
+            let listItem = document.createElement("ul");
+            listItem.innerHTML = `
+            <h1>${article.name}</h1>
+            <ul>
+                <li>${categoryType}</li>
+                <li>${article.description}</li>
+                <li>${article.price}€</li>
+                <li>${article.quantity} en stock</li>
+            </ul>
+        `;
+            articlesList.appendChild(listItem);
+            foundArticles = true;
+        });
+    });
+    if (!foundArticles) {
+        articlesList.innerHTML = 'Article non trouvé';
+    }
+}
+
+//displayArticles(jsonDatas);  //Afficher tous les contacts
+
+//Filtre les articles par nom et disponibilité
+button.addEventListener("click", function() {
+    let isChecked = check.checked;
+    let input = document.getElementById("type").value.toLowerCase();
+
+    let filteredCategories = [];
+
+    jsonDatas.forEach(category => {
+        // Tableau temporaire pour les items filtrés
+        let filteredItems = [];
+
+        // Parcourir chaque item de la catégorie
+        category.items.forEach(item => {
+            // Appliquer les conditions de filtrage
+            if (category.type.toLowerCase() === input && (!isChecked || item.quantity > 0)) {
+                filteredItems.push(item);
+            }
+        });
+
+        // Si on a trouvé des articles dans cette catégorie, on l'ajoute à filteredCategories
+        if (filteredItems.length > 0) {
+            filteredCategories.push({
+                typeTraduit: category.typeTraduit,
+                items: filteredItems
+            });
+        }
+    });
+    displayArticles(filteredCategories);
+});
+
+// Trier les articles par nom
+nameItem.addEventListener("click", function() {
+    let isChecked = document.getElementById("dsc").checked;
+
+    let allArticles = [];
+    jsonDatas.forEach(category => {
+        allArticles = allArticles.concat(category.items);
+    });
+
+    // Trier les articles en fonction de l'ordre sélectionné
+    let sortedArticles = allArticles.sort((a, b) => {
+        if (isChecked) {
+            return b.name.localeCompare(a.name); // Tri décroissant
+        }
+        return a.name.localeCompare(b.name); // Tri croissant
+    });
+
+    // Afficher les articles triés
+    displayArticles([{ typeTraduit: '', items: sortedArticles }]);
+});
+
+// Trier les articles par prix
+price.addEventListener("click", function() {
+    let isChecked = document.getElementById("dsc").checked;
+    let allArticles = [];
+    jsonDatas.forEach(category => {
+        allArticles = allArticles.concat(category.items);
+    });
+
+    // Trier les articles en fonction de l'ordre sélectionné
+    let sortedArticles = allArticles.sort((a, b) => {
+        if (isChecked) {
+            return b.price - a.price; // Tri décroissant
+        }
+        return a.price - b.price; // Tri croissant
+    });
+
+    // Afficher les articles triés
+    displayArticles([{ typeTraduit: '', items: sortedArticles }]);
+});
+
+// Ajout d'un nouvel article à la liste
+let form = document.getElementById("itemForm");
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById('name').value;
+    console.log("Formulaire name : " + name);
+    let type = document.getElementById('typeForm').value;
+    let description = document.getElementById('description').value;
+    let price = parseFloat(document.getElementById('price').value);
+    let quantity = parseInt(document.getElementById('quantity').value);
+    let lastName = document.getElementById('lastName').value;
+    let firstName = document.getElementById('firstName').value;
+    let address = document.getElementById('address').value;
+
+    let newItem = {
+        "name": name,
+        "description": description,
+        "price": price,
+        "quantity": quantity,
+        "contact": {
+            "lastName": lastName,
+            "firstName": firstName,
+            "address": address
+        }
+    };
+
+    let categoryFound = false;
+    jsonDatas.forEach(category => {
+        if (category.type === type) {
+            category.items.push(newItem);
+            categoryFound = true;
+        }
+    });
+
+    // Si aucune catégorie correspondante n'est trouvée, créer une nouvelle catégorie
+    if (!categoryFound) {
+        jsonDatas.push({
+            "type": type,
+            "items": [newItem]
+        });
+    }
 
 
+    console.log("Nouvel item ajouté : ", newItem);
+    console.log("Liste mise à jour : ", jsonDatas);
 
+    displayArticles(jsonDatas); // Affiche la liste mise à jour
+    form.reset();
 
+});
